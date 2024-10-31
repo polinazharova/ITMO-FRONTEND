cdbefa045fa800e41519fccfb2fec81e804a74c6
polinazharova <1132226165@pfur.ru>
2024-10-31 16:38:48 +0300

    script fixed

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,17 @@
 (() => {
     const loadStart = performance.now();
-    
-    const currentLocation = document.location.href;
-     const menuLinks = document.querySelectorAll('.main-navigation__year-link');
-     menuLinks.forEach(link => {
-        if (link.href === currentLocation) {
-            link.classList.add('main-navigation__year-link_active');
-        }
-    });
 
     window.addEventListener('load', () => {
         const loadEnd = performance.now();
         const footer = document.getElementsByClassName('footer__loading-info');
         footer[0].innerHTML = `LOAD TIME: ${(loadEnd - loadStart).toFixed(3)} ms`;
+
+        const currentLocation = document.location.href;
+        const menuLinks = document.querySelectorAll('.main-navigation__year-link');
+        menuLinks.forEach(link => {
+           if (link.href === currentLocation) {
+               link.classList.add('main-navigation__year-link_active');
+           }
+       });
     });
 })();
