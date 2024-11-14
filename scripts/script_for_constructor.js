ce87fab35c2e1ebd1046c329af318a0da63cb4e8
polinazharova <1132226165@pfur.ru>
2024-11-14 15:51:36 +0300

    js script fixed

--- scripts/script_for_constructor.js
+++ scripts/script_for_constructor.js
@@ -6,6 +6,48 @@ musicNamesStorage = [];
 count = 0;
 listened = 0;
 
+function cardCreation(text) {
+    cards = document.getElementById('cards');
+
+    card = document.createElement('div');
+    card.className = 'card';
+
+    radio_done = document.createElement('input');
+    radio_done.className = 'check-done';
+    radio_done.setAttribute('type', 'checkbox');
+    radio_done.setAttribute('name', 'done-not');
+    radio_done.setAttribute('id', 'done' + count);
+    card.appendChild(radio_done);
+
+
+    albumSongName = document.createElement('span');
+    albumSongName.className = 'album-song-name';
+    albumSongName.textContent = text;
+    card.appendChild(albumSongName);
+
+    cards.prepend(card);
+
+    count++;
+
+    radioListener(radio_done);
+}
+
+function radioListener(radio_done) {
+    radio_done.addEventListener('change', (event) => {
+        event.stopPropagation();
+
+        if (event.target.checked) {
+            listened++;
+        } else {
+            listened--;
+        }
+
+        document.getElementById('listened').textContent = `Прослушано: ${listened}`;
+        
+        localStorage.setItem(event.target.id, event.target.checked); 
+    });
+}
+
 if (localStorage.length > 0) {
 
     if (localStorage.getItem('musicNames')) {
@@ -13,27 +55,7 @@ if (localStorage.length > 0) {
     }
 
     for (i = 0; i < musicNamesStorage.length; i++) {
-        cards = document.getElementById('cards');
-
-        card = document.createElement('div');
-        card.className = 'card';
-    
-        radio_done = document.createElement('input');
-        radio_done.className = 'check-done';
-        radio_done.setAttribute('type', 'checkbox');
-        radio_done.setAttribute('name', 'done-not');
-        radio_done.setAttribute('id', 'done' + count);
-        card.appendChild(radio_done);
-    
-    
-        albumSongName = document.createElement('span');
-        albumSongName.className = 'album-song-name';
-        albumSongName.textContent = musicNamesStorage[i];
-        card.appendChild(albumSongName);
-
-        cards.prepend(card);
-
-        count++;
+        cardCreation(musicNamesStorage[i]);
     }
 
     document.getElementById('all').textContent = `Всего: ${count}`;
@@ -57,60 +79,10 @@ form.addEventListener('submit', event => {
     
     const formData = new FormData(form);
 
-    cards = document.getElementById('cards');
+    cardCreation(formData.get('listento'));
 
-    card = document.createElement('div');
-    card.className = 'card';
-
-    radio_done = document.createElement('input');
-    radio_done.className = 'check-done';
-    radio_done.setAttribute('type', 'checkbox');
-    radio_done.setAttribute('name', 'done-not');
-    radio_done.setAttribute('id', 'done' + count);
-    card.appendChild(radio_done);
-
-
-    albumSongName = document.createElement('span');
-    albumSongName.className = 'album-song-name';
-    albumSongName.textContent = formData.get('listento');
-    card.appendChild(albumSongName);
-    
-    cards.prepend(card);
-
-    radio_done.addEventListener('change', (event) => {
-        event.stopPropagation();
-
-        if (event.target.checked) {
-            listened++;
-        } else {
-            listened--;
-        }
-
-        document.getElementById('listened').textContent = `Прослушано: ${listened}`;
-        
-        localStorage.setItem(event.target.id, event.target.checked); 
-    });
-
-    count++;
     document.getElementById('all').textContent = `Всего: ${count}`;
 
     musicNamesStorage.push(formData.get('listento'));
     localStorage['musicNames'] = JSON.stringify(musicNamesStorage);
 });
-
-for (i = 0; i < checkboxes.length; i++) {
-    checkboxes[i].addEventListener('change', (event) => {
-        event.stopPropagation();
-
-        if (event.target.checked) {
-            listened++;
-        }
-        else {
-            listened--;
-        }
-
-        document.getElementById('listened').textContent = `Прослушано: ${listened}`;
-
-        localStorage[event.target.id] = event.target.checked;
-    });
-}
